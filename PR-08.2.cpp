4ac777bd859fc95d0669af154b0b4e96a5dc4aa1
Danylo <Danylo.Brudnyi.RI.2023@lpnu.ua>
2023-11-20 16:15:12 +0200

    first commit

---
+++ PR-08.2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+string Remove( string s)
+{
+    bool open = false;
+    string ns = "";
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == '(' && !open && s.find(")",i)!=-1) {
+            open = true;
+        }
+        if (!open)
+            ns += s[i];
+        if (s[i] == ')' && open) {
+            open = false;
+        }
+      
+    }
+  
+    return ns;
+}
+
+int main()
+{
+    string str;
+    cout << "Enter string:" << endl;
+    getline(cin, str);
+    cout << "Changed string: " << Remove(str) << endl;
+    return 0;
+}
